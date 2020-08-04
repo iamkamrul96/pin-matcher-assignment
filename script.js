d1e2ec553c84d9e84992bf7201200cd34819d82d
iamkamrul96 <kamrulchowdhury51@gmail.com>
2020-08-04 22:50:55 +0600

    pin-assignment-done

---
+++ script.js
@@ -0,0 +1,109 @@
+// Calculator Button work
+$(document).ready(function () {
+
+    // VARIABLES
+    var calc = $('.container');
+    var calcDisplay = calc.find('.calculator__display');
+    var calcKeys = calc.find('.calculator__key');
+    var calcButton = calc.find('.calculator__button');
+    var calcClear = calc.find('.calculator__clear');
+    var calcEqual = calc.find('.calculator__key--equal');
+    var calcPower = calc.find('.calculator__power');
+    var calcSpace = calc.find('.calculator__backspace');
+
+    // INIT CALC KEYS
+    calcKeys.each(function () {
+        var current = $(this).attr('value');
+        $(this).text(current);
+    });
+
+    // ADD NUMBERS TO INPUT
+    calcButton.on('click', function () {
+        calcDisplay.val(calcDisplay.val() + $(this).attr('value'));
+    });
+
+    // CLEAR INPUT
+    calcClear.on('click', function () {
+        calcDisplay.val('');
+    });
+
+    // SHOW RESULT
+    calcEqual.on('click', function () {
+        calcDisplay.val(eval(calcDisplay.val()));
+    });
+
+    // POWER BUTTON
+    calcPower.on('click', function () {
+        calcDisplay.val(Math.pow(calcDisplay.val(), 3));
+    });
+
+    // BACKSPACE BUTTON
+    calcSpace.on('click', function () {
+        calcDisplay.val(calcDisplay.val().substring(0, calcDisplay.val().length - 1));
+    });
+
+});
+
+
+/// GeneratePin & inputPin work from submit
+
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", function myFunction() {
+
+    let generatePin = document.getElementById("displayOne").value;
+
+    let inputPin = document.getElementById("displayTwo").value;
+
+    //create for loop to choose password characters
+    if (generatePin.length == 4 && inputPin == generatePin) {
+        document.getElementById("match").style.visibility = "visible";
+
+    } else {
+        document.getElementById("unmatch").style.visibility = "visible";
+
+    }
+});
+
+
+// Generate pin & Random number output work done
+const generateNumber = document.getElementById("generate");
+generateNumber.addEventListener("click", function () {
+    let randomNumber = "";
+
+    //create for loop to choose password characters
+    for (var i = 0; i <= length; i++) {
+        randomNumber = Math.floor(1000 + Math.random() * 9000);
+    }
+    //add password to textbox/display area
+    document.getElementById("displayOne").value = randomNumber;
+})
+
+// hide input with added placeholder done
+const hideBtn = document.querySelector(".submit-btn");
+hideBtn.addEventListener("click", function () {
+    document.getElementById("displayOne").value = "";
+    document.getElementById("displayTwo").value = "";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
